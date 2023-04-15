70690b08291860afe9b42a7adfa88ee91520057a
sohanemon <m.sohanemon@gmail.com>
2023-04-15 21:36:51 +0600

    donateToCampaign done

--- contracts/moving-trends.sol
+++ contracts/moving-trends.sol
@@ -61,4 +61,17 @@ contract MovingTrends {
         }
         return allCampaign;
     }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        // check the transaction
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 }
