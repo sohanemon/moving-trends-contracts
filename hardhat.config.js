c5dcf3856cd6c5fff8eaa8c0e404c57a350f4514
sohanemon <m.sohanemon@gmail.com>
2023-04-15 22:12:40 +0600

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    // added by me
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://rpc.sepolia.dev',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
+    // end
     settings: {
       optimizer: {
         enabled: true,
