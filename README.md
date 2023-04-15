ea0af8bd749e7526b51d73eb94d6ed6b717d8199
sohanemon <m.sohanemon@gmail.com>
2023-04-15 13:09:03 +0600

    createCampaign done

--- README.md
+++ README.md
@@ -3,3 +3,45 @@
 ```bash
 npx thirdweb create --contract
 ```
+
+## `struct` - the `type` keyword of solidity
+
+a `struct` in Solidity can be compared to a `type` in TypeScript.
+Both allow you to define your own custom data types with multiple properties.
+
+```csharp
+struct Campaign {
+        address owner;
+        string title;
+        uint256 deadline;
+        string image;
+        address[] donators;
+    }
+```
+
+## `mapping`
+
+```csharp
+mapping(address => uint) public balances;
+```
+
+`address` is the key type and `uint` is the value type. And the variable name is `balances`.
+
+And will be used as like `array`
+
+```csharp
+balances[something] = 100;
+```
+
+## `memory` keyword
+
+If you pass a very large variable, like an array, as a parameter to a function, storing that data permanently on the blockchain can be very expensive in terms of gas fees. This is where "memory" comes in.
+
+When you add "memory" to a function parameter, you're telling Solidity to store that variable's data temporarily in memory while the function is being executed.
+
+```csharp
+ function createCampaign(
+        address _owner,
+        string memory _title
+    ) public returns (uint256) {}
+```
--- contracts/moving-trends.sol
+++ contracts/moving-trends.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract MovingTrends {
+    //
     struct Campaign {
         address owner;
         string title;
@@ -12,5 +13,36 @@ contract MovingTrends {
         string image;
         address[] donators;
         uint256[] donations;
+    }
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    // create campaigns
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _desc,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        // creating a campaign of type Campaign and storing on blockchain
+
+        require(
+            campaign.deadline < block.timestamp,
+            "deadline should be in the future"
+        );
+        // filling our campaign
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.desc = _desc;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+        numberOfCampaigns++;
+        return numberOfCampaigns - 1;
     }
 }
